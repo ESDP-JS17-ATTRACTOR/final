b0b105614e97c498da4f31029ccfde36ba68980c
Almaz Balbaev <kujitengu@gmail.com>
2023-04-15 17:15:19 +0600

    Creating a basic project structure

---
+++ tests/codecept.conf.ts
@@ -0,0 +1,19 @@
+export const config: CodeceptJS.MainConfig = {
+  tests: './*_test.ts',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost:3000/',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {},
+  "gherkin": {
+    "features": "./features/*.feature",
+    "steps": [
+      "./step_definitions/steps.ts"
+    ]
+  },
+  name: 'tests'
+}

50d91a690aceefce83f40a3cd1a63f291ef7994d
Altyn Almazbekova <122968390+altynaa@users.noreply.github.com>
2023-04-21 20:15:29 +0600

    Update LanguageSwitcher.tsx
    Edited default value to select and options.

--- front/src/components/UI/LanguageSwitcher.tsx
+++ front/src/components/UI/LanguageSwitcher.tsx
@@ -5,7 +5,6 @@ const LanguageSwitcher = () => {
   const router = useRouter();
   const { pathname, asPath, query } = router
   const [locale, setLocale] = useState<string>(router.locale as string);
-  const isTrue = locale === 'en';
 
   const handleLanguageChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
     const newLocale = e.target.value;
@@ -21,10 +20,11 @@ const LanguageSwitcher = () => {
         className="language-switcher"
         name="languageSwitcher"
         id="languageSwitcher"
+        defaultValue={locale}
         onChange={handleLanguageChange}
       >
-        <option value='en' selected={isTrue} disabled={isTrue}>EN</option>
-        <option value="ru" selected={!isTrue} disabled={!isTrue}>RU</option>
+        <option value='en' disabled={locale === "en"}>EN</option>
+        <option value="ru" disabled={locale === "ru"}>RU</option>
       </select>
     </div>
   );

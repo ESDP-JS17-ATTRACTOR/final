da7561cf20ab4d5134b7388c527db5c16838eefc
Almaz Balbaev <kujitengu@gmail.com>
2023-05-13 17:07:42 +0600

    #31 Register dto & user entity edit - edited validation decorators

--- back/src/users/dto/register.dto.ts
+++ back/src/users/dto/register.dto.ts
@@ -1,15 +1,18 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
   firstName: string;
 
   @IsNotEmpty()
+  @IsString()
   lastName: string;
 
   @IsNotEmpty()
+  @IsString()
   password: string;
 }

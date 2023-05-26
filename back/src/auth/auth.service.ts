b4af2299fc2ea72f8eefe652dc7f0afec88c542d
Aidai Dzhunushalieva <aidaimond5@gmail.com>
2023-05-26 16:08:03 +0600

    #13 Backend: Installed @nest/jwt. Added in user.entity.ts facebookId and linkedInId. Created generateJwtToken in auth.service.

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -4,11 +4,14 @@ import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { randomUUID } from 'crypto';
 import axios from 'axios';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
-
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
   async validateUser(email: string, password: string) {
     const user = await this.userRepository.findOne({ where: { email } });
 
@@ -91,4 +94,56 @@ export class AuthService {
 
     return user;
   }
+
+  generateJwtToken(user: User): string {
+    const payload = {
+      userId: user.id,
+      email: user.email,
+    };
+    const options: JwtSignOptions = {
+      secret: process.env.JWT_SECRET, // Используйте свойство "secret" вместо "secretKey"
+    };
+    return this.jwtService.sign(payload, options);
+  }
+  // async registerUserWithFacebook(accessToken: string) {
+  //   try {
+  //     const response = await axios.get(
+  //       `https://graph.facebook.com/v13.0/me?fields=email,first_name,last_name,picture&access_token=${accessToken}`,
+  //     );
+  //
+  //     console.log('Response from Facebook API:', response.data);
+  //     const email = response.data.email;
+  //     const facebookId = response.data.id;
+  //     const firstName = response.data.first_name;
+  //     const lastName = response.data.last_name;
+  //     const avatar = response.data.picture?.data.url;
+  //
+  //     if (!email) {
+  //       return new BadRequestException('Not enough user data to continue.');
+  //     }
+  //
+  //     let user = await this.userRepository.findOne({
+  //       where: { facebookId },
+  //     });
+  //
+  //     if (!user) {
+  //       user = await this.userRepository.create({
+  //         email,
+  //         firstName,
+  //         lastName,
+  //         facebookId,
+  //         avatar,
+  //         password: crypto.randomUUID(),
+  //       });
+  //
+  //       await user.generateToken();
+  //       await this.userRepository.save(user);
+  //       return user;
+  //     }
+  //
+  //     return user;
+  //   } catch (e) {
+  //     throw new BadRequestException(e.message);
+  //   }
+  // }
 }

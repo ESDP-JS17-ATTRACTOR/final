17fb0ca1b0e16e8cebbbfc0841d3fd2b06f079f9
Aidai Dzhunushalieva <aidaimond5@gmail.com>
2023-05-26 16:11:15 +0600

    #13 Backend: Created registerUserWithFacebook in controller

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   ClassSerializerInterceptor,
   Controller,
@@ -19,6 +20,7 @@ import { TokenAuthGuard } from '../auth/token-auth.guard';
 import { AuthService } from '../auth/auth.service';
 import { LocalAuthGuard } from '../auth/local-auth.guard';
 import { CurrentUser } from '../auth/currentUser.decorator';
+import axios from 'axios';
 
 @Controller('users')
 export class UsersController {
@@ -70,5 +72,53 @@ export class UsersController {
       where: { role: 'tutor' },
       select: ['id', 'firstName', 'lastName', 'role'],
     });
+  }
+  @Post('facebook-authentication')
+  @UseInterceptors(ClassSerializerInterceptor)
+  @UsePipes(ValidationPipe)
+  async registerUserWithFacebook(@Body() body: { accessToken: string; userID: string }) {
+    try {
+      const accessToken = body.accessToken;
+      const userID = body.userID;
+
+      const facebookResponse = await axios.get(
+        `https://graph.facebook.com/v12.0/${userID}?fields=name,email,picture,first_name,last_name&access_token=${accessToken}`,
+      );
+      console.log(facebookResponse.data);
+      const email = facebookResponse.data.email;
+      const facebookId = facebookResponse.data.id;
+      const firstName = facebookResponse.data.first_name;
+      const lastName = facebookResponse.data.last_name;
+      const avatar = facebookResponse.data.picture?.data.url;
+
+      if (!email) {
+        return new BadRequestException('Not enough user data to continue.');
+      }
+
+      let user = await this.userRepository.findOne({
+        where: { facebookId },
+      });
+
+      if (!user) {
+        user = await this.userRepository.create({
+          email,
+          firstName,
+          lastName,
+          facebookId,
+          avatar,
+          password: crypto.randomUUID(),
+        });
+        const jwtToken = this.authService.generateJwtToken(user);
+
+        await user.generateToken();
+        await this.userRepository.save(user);
+        return { user, jwtToken };
+      }
+      const jwtToken = this.authService.generateJwtToken(user);
+
+      return { user, jwtToken };
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
   }
 }

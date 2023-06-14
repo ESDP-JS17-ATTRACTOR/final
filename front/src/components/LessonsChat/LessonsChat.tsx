d4538b31d2e05a1130bc0146ffb1d0ef7968fb33
Almaz Balbaev <kujitengu@gmail.com>
2023-06-14 16:28:08 +0600

    #33 Frontend: changes on ws chat logic

--- front/src/components/LessonsChat/LessonsChat.tsx
+++ front/src/components/LessonsChat/LessonsChat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import DefaultAvatar from '../../../public/avatar.png';
 import { useAppSelector } from '@/app/hooks';
@@ -6,6 +6,7 @@ import { selectUser } from '@/features/users/usersSlice';
 import { useRouter } from 'next/router';
 import LessonsChatMessage from '@/components/LessonsChat/LessonsChatMessage';
 import SendIcon from '@mui/icons-material/Send';
+import { ChatMsg } from '../../../types';
 
 const LessonsChat = () => {
   const user = useAppSelector(selectUser);
@@ -13,47 +14,67 @@ const LessonsChat = () => {
   const socket = io('http://localhost:8000');
   const router = useRouter();
   const { id } = router.query;
+  const [msgState, setMsgState] = useState<ChatMsg[]>([]);
 
   useEffect(() => {
     socket.on('connect', () => {
-      socket.emit('getAllMessages', id);
+      socket.emit('getAllMessages');
     });
 
     socket.on('disconnect', () => {
       console.log('Connection is off');
     });
 
-    // socket.on('message', ({ data })) => {
-    //
-    // }
+    socket.on('allMessages', (messages: ChatMsg[]) => {
+      setMsgState(messages);
+    });
+
+    socket.on('newMessage', (newMessage: ChatMsg) => {
+      setMsgState((prevMessages) => [...prevMessages, newMessage]);
+    });
 
     return () => {
       socket.disconnect();
     };
   }, [socket, id]);
 
+  const sendMessage = () => {
+    const message = 'Новое сообщение';
+    socket.emit('message', { message });
+  };
+
   return (
     <>
       <div>
-        <LessonsChatMessage
-          userId={2}
-          displayName={'Jake'}
-          avatar={'asd'}
-          message={'text message'}
-          createdAt={'2023-05-03 20:19:17.937+06'}
-        />
-        <LessonsChatMessage
-          userId={1}
-          displayName={'John'}
-          avatar={'asd'}
-          message={'etext messagetext messagetext messagetext messagetext messagetext message'}
-          createdAt={'2023-05-03 20:19:17.937+06'}
-        />
+        {msgState.map((message) => (
+          <LessonsChatMessage
+            key={message.id}
+            userId={message.userId}
+            displayName={message.displayName}
+            avatar={message.avatar}
+            message={message.message}
+            createdAt={message.createdAt}
+          />
+        ))}
+        {/*<LessonsChatMessage*/}
+        {/*  userId={2}*/}
+        {/*  displayName={'Jake'}*/}
+        {/*  avatar={'asd'}*/}
+        {/*  message={'text message'}*/}
+        {/*  createdAt={'2023-05-03 20:19:17.937+06'}*/}
+        {/*/>*/}
+        {/*<LessonsChatMessage*/}
+        {/*  userId={1}*/}
+        {/*  displayName={'John'}*/}
+        {/*  avatar={'asd'}*/}
+        {/*  message={'etext messagetext messagetext messagetext messagetext messagetext message'}*/}
+        {/*  createdAt={'2023-05-03 20:19:17.937+06'}*/}
+        {/*/>*/}
       </div>
       {/*<Image src={avatar} className="comment-form_user-avatar" alt="avatar" width={100} height={100} />*/}
       <div className="input-block">
         <input type="text" />
-        <SendIcon className="input-block-send-icon" fontSize="large" />
+        <SendIcon className="input-block-send-icon" fontSize="large" onClick={sendMessage} />
       </div>
     </>
   );

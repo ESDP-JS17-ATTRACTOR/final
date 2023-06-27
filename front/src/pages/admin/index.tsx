61928b12d1bbfaf2b321bc0ad8457f81881c3451
Altyn Almazbekova <almazbekova.altyn@gmail.com>
2023-06-27 13:04:38 +0600

    #56 Frontend: - updated catalogs component

--- front/src/pages/admin/index.tsx
+++ front/src/pages/admin/index.tsx
@@ -2,7 +2,6 @@ import React, { PropsWithChildren } from 'react';
 import SideBar from '@/components/UI/Admin/SideBar';
 import { Box, Container, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import IsAdmin from '@/components/UI/Auth/IsAdmin';
 
 const Admin: React.FC<PropsWithChildren> = ({ children }) => {
   const mdTheme = createTheme();
@@ -35,4 +34,4 @@ const Admin: React.FC<PropsWithChildren> = ({ children }) => {
   );
 };
 
-export default IsAdmin(Admin);
+export default Admin;

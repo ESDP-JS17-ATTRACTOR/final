6173531e491caa4e9b7a508bd8da83fe3c742d1e
Aibek_Saaliev <dokaidea@gmail.com>
2023-06-28 15:58:52 +0600

    ESLINT: Fixed errors

--- front/src/pages/admin/courses/course/[id].tsx
+++ front/src/pages/admin/courses/course/[id].tsx
@@ -76,10 +76,7 @@ const Course = () => {
             </TableHead>
             <TableBody>
               {lessons.map((lesson) => (
-                <TableRow
-                  key={lesson.id}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
+                <TableRow key={lesson.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                   <TableCell>
                     #{lesson.module.number} {lesson.module.title}
                   </TableCell>

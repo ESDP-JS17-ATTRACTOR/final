7a77f61e7432df6e8b298592f4651de49516c278
Aidai Dzhunushalieva <aidaimond5@gmail.com>
2023-06-28 17:18:16 +0600

    #64 Frontend: Added props for StudentWorkPreviewCard and TeamCard. Added 2 images for team-block.

--- front/src/components/Cards/StudentWorkPreviewCard.tsx
+++ front/src/components/Cards/StudentWorkPreviewCard.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const StudentWorkPreviewCard = () => {
+interface Props {
+  image: string;
+}
+
+const StudentWorkPreviewCard: React.FC<Props> = ({ image }) => {
   return (
     <div className="students-work_card">
       <div className="students-work_card_info">
-        <Image src="/work-card-1.png" alt="image" width={307} height={295}/>
+        <Image src={image} alt="image" width={307} height={295} />
         <div className="students-work_card_info_description">
           <h6>Volfram web</h6>
           <Link href="/students-works">learn more</Link>
--- front/src/components/Cards/TeamCard.tsx
+++ front/src/components/Cards/TeamCard.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Image from 'next/image';
 
-const TeamCard = () => {
+interface Props {
+  image: string;
+  name: string;
+  position: string;
+  info: string;
+}
+
+const TeamCard: React.FC<Props> = ({ image, name, position, info }) => {
   return (
     <div className="team-card_bg">
       <div className="team-card">
         <div className="team-card_photo">
-          <Image src="/team-man.png" alt="Photo" width={172} height={167} />
+          <Image src={image} alt="Photo" width={172} height={167} />
         </div>
         <div className="team-card_info">
-          <span className="team-card_info_name">Nino</span>
-          <span className="team-card_info_job-title">Project Manager</span>
-          <p className="team-card_info_description">
-            With her extensive background in managing IT teams, she proudly succeeds in all her projects. She has a side
-            passion for writing fiction stories, playing guitar, singing, and traveling.
-          </p>
+          <span className="team-card_info_name">{name}</span>
+          <span className="team-card_info_job-title">{position}</span>
+          <p className="team-card_info_description">{info}</p>
         </div>
       </div>
     </div>
